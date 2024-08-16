32c9f6973dad1456d8c227e0087f6dc846d12721
Axel Alvarado <aalvaradoaxel@outlook.com>
2024-08-16 15:42:44 -0400

    Remove comments in main.py

--- main.py
+++ main.py
@@ -5,18 +5,7 @@ student_dict = {
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 data_frame = pandas.DataFrame(data)
